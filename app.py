dcf06383a27e52e552aafcbdf7405f4eda6ef32a
VENKYS48 <venkateshpingalkar48@gmail.com>
2023-11-28 22:44:44 +0530

    updating the backend

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json
+from flask_cors import CORS
 import pickle
 import warnings
 
 app = Flask(__name__)
+CORS(app)
 
 warnings.filterwarnings('ignore')
 
@@ -13,7 +15,7 @@ loaded_model = pickle.load(open(MODEL_PATH, 'rb'))
 # Render the main page
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return json.dumps({ "message": "Okay" })
 
 # Calculate and return the sales prediction
 @app.route('/predict', methods=['POST'])
@@ -22,10 +24,12 @@ def predict():
         tv = float(request.form['tv'])
         radio = float(request.form['radio'])
         newspaper = float(request.form['newspaper'])
+
+        print(tv, radio, newspaper)
         
         res = loaded_model.predict([[tv, radio, newspaper]])
         predicted_sale = "{:.2f}".format(res[0][0])
-        return render_template('result.html', sale=predicted_sale)
+        return json.dumps({ "result": predicted_sale })
 
 if __name__ == '__main__':
     app.run(debug=True)
